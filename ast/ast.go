3bc57fbe8e04bed344d39b78852905909673ea8f
Rohan Bera <104846134+hotliquid@users.noreply.github.com>
2024-06-28 21:37:45 +0530

    AST

---
+++ ast/ast.go
@@ -0,0 +1,41 @@
+package ast
+
+import "go/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
